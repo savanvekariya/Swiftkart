071d5cb43841e01d4fa14aa942127c2bce6c93a0
Savan Vekariya <savanvekariya.sv@gmail.com>
2023-05-27 22:41:35 +0200

    express server and initial product routes

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import products from './data/products.js';
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  return res.json(products);
+});
+
+app.get('/api/product/:id', (req, res) => {
+  const product = products.find((item) => item._id === req.params.id);
+  return res.json(product);
+});
+
+app.listen(port, () => console.log(`server is running on ${port}`));
