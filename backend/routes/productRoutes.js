8c957f19336743f67e0d0031b2192bd9cec03206
Savan Vekariya <savanvekariya.sv@gmail.com>
2023-05-31 22:52:06 +0200

    import controller functions

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import {
+    getProducts,
+    getProductById,
+  } from '../controllers/productController.js';
 
 router.route('/').get(getProducts);
 router.route('/:id').get(getProductById);
