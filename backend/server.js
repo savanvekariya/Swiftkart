d8324ae999732794bb51d024ac077c21205725f6
Savan Vekariya <savanvekariya.sv@gmail.com>
2023-05-31 15:25:18 +0200

    Error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 const port = process.env.PORT;
 
 connectDB();
 
 const app = express();
 
+app.use('/api/products', productRoutes);
+
 app.get('/', (req, res) => {
   return res.send('API is running...');
 });
 
-app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server is running on ${port}`));
