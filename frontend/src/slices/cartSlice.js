2c57e089545f7772bea1fad1b46e357b39ce1f70
Savan Vekariya <savanvekariya.sv@gmail.com>
2023-06-02 16:46:46 +0200

    clean up cartSlice and created utils

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,12 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] };
-
-  const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2);
-  };
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -16,41 +13,7 @@ const cartSlice = createSlice({
       // The item to add to the cart
       const item = action.payload;
 
-      // check if item is already in the cart
-      const existItem = state.cartItems.find(
-        (cartItem) => cartItem._id === item._id
-      );
-
-      if (existItem) {
-        state.cartItems = state.cartItems.map((cartItem) =>
-          cartItem._id === existItem._id ? item : cartItem
-        );
-      } else {
-        state.cartItems = [...state.cartItems, item];
-      }
-
-      // Calculate the items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      );
-
-      // Calculate the shipping price | If items price is greater than 100, shipping is free | If not, shipping is 10
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-
-      // Calculate the tax price | Tax is 15% of the items price
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2))
-      );
-
-      // Calculate the total price | Total price is the sum of the items price, shipping price and tax price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      // Save the cart to localStorage
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state, item);
     },
   },
 });
