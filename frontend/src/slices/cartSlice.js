9252b8395deed3fa00f33a75412f9ef152f0a21c
Savan Vekariya <savanvekariya.sv@gmail.com>
2023-06-05 16:02:08 +0200

    added shipping screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
-  : { cartItems: [] };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -13,18 +13,18 @@ const cartSlice = createSlice({
       // The item to add to the cart
       const item = action.payload;
 
-            // Check if the item is already in the cart
-            const existItem = state.cartItems.find((x) => x._id === item._id);
+      // Check if the item is already in the cart
+      const existItem = state.cartItems.find((x) => x._id === item._id);
 
-            if (existItem) {
-              // If exists, update quantity
-              state.cartItems = state.cartItems.map((x) =>
-                x._id === existItem._id ? item : x
-              );
-            } else {
-              // If not exists, add new item to cartItems
-              state.cartItems = [...state.cartItems, item];
-            }
+      if (existItem) {
+        // If exists, update quantity
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        // If not exists, add new item to cartItems
+        state.cartItems = [...state.cartItems, item];
+      }
 
       return updateCart(state, item);
     },
@@ -35,9 +35,14 @@ const cartSlice = createSlice({
       // Update the prices and save to storage
       return updateCart(state);
     },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
