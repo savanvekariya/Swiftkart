137c8db4ff46f6a1349c9d5d6866e4be9080a208
Savan Vekariya <savanvekariya.sv@gmail.com>
2023-07-04 22:58:13 +0200

    Product carousel route and controller function

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,7 +6,8 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
-  createProductReview
+  createProductReview,
+  getTopProducts
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -17,5 +18,6 @@ router
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
   router.route('/:id/reviews').post(protect, createProductReview);
+  router.get('/top', getTopProducts);
 
 export default router;
